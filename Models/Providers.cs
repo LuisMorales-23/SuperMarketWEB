15b8d29205956d57bb7adb1068a2eaa5e6f220a1
LuisMorales-23 <dev01.Luis.morales@gmail.com>
2024-11-02 12:27:55 -0500

    create provider

--- Models/Providers.cs
+++ Models/Providers.cs
@@ -3,8 +3,8 @@
     public class Providers
     {
         public int Id { get; set; } // será la llave primaria
-        public string Name { get; set; }
-        public int Number { get; set; }
-        public string Observation { get; set; }
+        public string? Name { get; set; }
+        public int? Number { get; set; }
+        public string? Observation { get; set; }
     }
 }
---
+++ Pages/Providers/Create.cshtml.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SuperMarketWEB.Data;
+using SuperMarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Provider
+{
+    public class CreateModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public CreateModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+
+        public Providers Provider { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.Providers == null || Provider == null)
+            {
+                return Page();
+            }
+            _context.Providers.Add(Provider);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
